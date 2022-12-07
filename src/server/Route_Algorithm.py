07d068ed2e73009c6585f07902fd5e00cf2a67a9
Zihao Zhang <zihaozhang@umass.edu>
2022-12-06 23:36:10 -0500

    initial astar func, waiting to implement

--- src/server/Route_Algorithm.py
+++ src/server/Route_Algorithm.py
@@ -2,11 +2,16 @@ import numpy as np
 import osmnx as ox
 import geopandas as gpd
 from shapely.geometry import Point
+from keys import google_elevation_api_key
+from utils import *
 
 
-def find_route(source, dest, place):
+def find_route(source, dest, place, method, percentage, min_max):
     # place = "Piedmont, California, USA"
     G = ox.graph_from_place(place, network_type="all")
+    G = ox.elevation.add_node_elevations_google(
+        G, api_key=google_elevation_api_key)
+    G = ox.elevation.add_edge_grades(G)
     print(source)
     print(dest)
     orig_node = ox.nearest_nodes(G, source[0], source[1])
@@ -16,18 +21,40 @@ def find_route(source, dest, place):
 
     # dest_node = ox.nearest_nodes((dest['x'], dest['y']))
     route = ox.shortest_path(G, orig_node, dest_node, weight="length")
+    route_length = get_path_length(G, route)
+
     routes = []
 
     for node in route:
         temp = [G.nodes()[node]['x'], G.nodes()[node]['y']]
         routes.append(temp)
-    return routes
+    return routes, route_length
 
 
-# if __name__ == "__main__":
-#     place = "Amherst, Massachusetts, USA"
-#     source = {'x': -72.5198118276834, 'y':  42.373051188825855}
-#     dest = {'x': -72.4992091462399, 'y': 42.36979729154845}
+def Astar(source, dest, min_max, percentage, place):
+    G = ox.graph_from_place(place, network_type="all")
+    G = ox.elevation.add_node_elevations_google(
+        G, api_key=google_elevation_api_key)
+    G = ox.elevation.add_edge_grades(G)
+    start = ox.nearest_nodes(G, source[0], source[1])
+    end = ox.nearest_nodes(G, dest[0], dest[1])
+    route1 = ox.shortest_path(G, start, end, weight="length")
+    route2 = ox.shortest_path(G, start, end, weight="impedance")
+    route1_length = get_path_length(G, route1)
+    print("asd")
+    return route1, route1_length
 
-#     routes = find_route(source, dest, place)
-#     print("done!")
+
+if __name__ == "__main__":
+    place = "Amherst, Massachusetts, USA"
+    source = [-72.5198118276834, 42.373051188825855]
+    dest = [-72.4992091462399, 42.36979729154845]
+
+    route1, r1_length = find_route(source, dest, place)
+    route2, r2_length = Astar(source, dest, 1, 2, place)
+
+    if (route1 == route2):
+        print("same")
+    else:
+        print("different")
+    print("done!")
--- src/server/utils.py
+++ src/server/utils.py
@@ -4,8 +4,21 @@ def get_length(G, start, end):
 
 def get_path_length(G, node_list):
     length = 0
-
     for i in range(len(node_list) - 1):
         length += get_length(G, node_list[i], node_list[i + 1])
-
     return length
+
+
+def get_path_elevation(G, node_list):
+    total_elevation = 0
+    for i in range(len(node_list) - 1):
+        curr_elevation = get_elevation_gain(G, node_list[i], node_list[i + 1])
+        if curr_elevation > 0:
+            total_elevation += curr_elevation
+    return total_elevation
+
+
+def get_elevation_gain(G, start, end):
+    if start == end:
+        return 0
+    return G.nodes[start]['elevation'] - G.nodes[end]['elevation']
