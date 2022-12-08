673c449e208bd475476979274efa8f3bfb526442
haorusong <haorusong@gmail.com>
2022-12-08 07:21:03 -0500

    elevation demo

--- src/server/Route_Algorithm.py
+++ src/server/Route_Algorithm.py
@@ -5,6 +5,7 @@ import geopandas as gpd
 from shapely.geometry import Point
 from keys import google_elevation_api_key
 from utils import *
+from pqdict import pqdict
 import networkx as nx
 
 
@@ -108,6 +109,53 @@ def dijkstra_find_route(geodata, orig, dest):
         path.append(current_node)
         current_node = prev[current_node]
     return path[::-1], 0, 0
+
+    # get_elevation_gain
+    #
+def get_neighbor_idx(x,y,dims):
+    res = []
+    for i in ([0,-1,1]):
+        for j in ([0,-1,1]):
+            if i==0 and j==0: continue
+            if x+i<(dims[0]) and x+i>-1 and y+j<(dims[1]) and y+j>-1:
+                res.append((x+i,y+j))
+    return res
+
+def dijkstra(C,s,e):    
+    D = {}
+    P = {}
+    Q = pqdict() 
+    Q[s] = 0
+
+    while len(Q)>0:
+        (v,vv) = Q.popitem()
+        D[v] = vv
+        neighs = get_neighbor_idx(v[0],v[1],C.shape)
+        for w in neighs:
+            vwLength = D[v] + np.abs(C[v[0],v[1]] - C[w[0],w[1]])
+            if w in D:
+                if vwLength < D[v]:
+                    raise ValueError
+            elif w not in Q or vwLength < Q[w]:
+                Q[w] = vwLength
+                P[w] = v
+
+    path = []
+    while 1:
+       path.append(e)
+       if e == s: break
+       e = P[e]
+    path.reverse()
+    return path   
+
+m = np.array([[999.9, 999.9, 999.9,   0. ],
+              [999.9, 999.9, 999.9,   0. ],
+              [999.9, 999.9, 999.9,   0. ],
+              [  0.,    0.,    0.,    0. ]])
+
+res = dijkstra(m,(3,0),(0,3))
+
+print (res)
 
 # if __name__ == "__main__":
 #     place = "Amherst, Massachusetts, USA"
