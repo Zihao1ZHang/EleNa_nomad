6a3b237a4e7ffc55e1e36ab40dcdbfb885c582e1
Zihao Zhang <zihaozhang@umass.edu>
2022-12-07 19:56:54 -0500

    added astar alogrithm

--- src/server/Route_Algorithm.py
+++ src/server/Route_Algorithm.py
@@ -1,12 +1,14 @@
 import numpy as np
+import heapq
 import osmnx as ox
 import geopandas as gpd
 from shapely.geometry import Point
 from keys import google_elevation_api_key
 from utils import *
+import networkx as nx
 
 
-def find_route(source, dest, place, method, percentage, min_max):
+def find_route(source, dest, place, method='A', percentage=1, min_max=1):
     # place = "Piedmont, California, USA"
     G = ox.graph_from_place(place, network_type="all")
     G = ox.elevation.add_node_elevations_google(
@@ -21,40 +23,78 @@ def find_route(source, dest, place, method, percentage, min_max):
 
     # dest_node = ox.nearest_nodes((dest['x'], dest['y']))
     route = ox.shortest_path(G, orig_node, dest_node, weight="length")
+
+    routes = get_route_coord(G, route)
     route_length = get_path_length(G, route)
+    elevation_g = get_path_elevation(G, route)
 
-    routes = []
-
-    for node in route:
-        temp = [G.nodes()[node]['x'], G.nodes()[node]['y']]
-        routes.append(temp)
-    return routes, route_length
+    return routes, route_length, elevation_g
 
 
-def Astar(source, dest, min_max, percentage, place):
+def Astar(source, dest, min_max, max_length, place):
+    # intial the map
     G = ox.graph_from_place(place, network_type="all")
     G = ox.elevation.add_node_elevations_google(
         G, api_key=google_elevation_api_key)
+
     G = ox.elevation.add_edge_grades(G)
+
+    # create source and destination node
     start = ox.nearest_nodes(G, source[0], source[1])
     end = ox.nearest_nodes(G, dest[0], dest[1])
-    route1 = ox.shortest_path(G, start, end, weight="length")
-    route2 = ox.shortest_path(G, start, end, weight="impedance")
-    route1_length = get_path_length(G, route1)
-    print("asd")
-    return route1, route1_length
+
+    open_list = [NodeWrapper(start)]
+    close_list = set()
+    visited_node = {start: NodeWrapper(start)}
+    while (len(open_list) > 0):
+        curr_node = heapq.heappop(open_list)
+        close_list.add(curr_node.id)
+
+        if (curr_node.id == end):
+            path = []
+            curr = curr_node
+            while curr.parent is not None:
+                path.insert(0, curr.id)
+                curr = visited_node[curr.parent]
+            path.insert(0, curr.id)
+            routes = get_route_coord(G, path)
+            route_length = get_path_length(G, path)
+            elevation_g = get_path_elevation(G, path)
+            return routes, route_length, elevation_g
+
+        successors = filter(lambda n: n not in close_list,
+                            nx.neighbors(G, curr_node.id))
+        for successor in successors:
+            distance = curr_node.curr_dist + \
+                get_length(G, curr_node.id, successor)
+            if distance <= max_length:
+                flag = successor in visited_node
+                pred_distance = distance + \
+                    get_heuristic_distance(G, successor, end)
+                elevation_gain = curr_node.elevation + \
+                    get_elevation_gain(G, curr_node.id, successor)
+                successor_node = NodeWrapper(
+                    successor, curr_node.id, distance, pred_distance, elevation_gain, open_list)
+                visited_node[successor] = successor_node
+
+                if flag:
+                    heapq.heapify(open_list)
+                else:
+                    heapq.heappush(open_list, successor_node)
+    return []
 
 
-if __name__ == "__main__":
-    place = "Amherst, Massachusetts, USA"
-    source = [-72.5198118276834, 42.373051188825855]
-    dest = [-72.4992091462399, 42.36979729154845]
+# if __name__ == "__main__":
+#     place = "Amherst, Massachusetts, USA"
+#     source = [-72.5198118276834, 42.373051188825855]
+#     dest = [-72.4992091462399, 42.36979729154845]
 
-    route1, r1_length = find_route(source, dest, place)
-    route2, r2_length = Astar(source, dest, 1, 2, place)
+#     route1, r1_length, elevation1 = find_route(source, dest, place)
+#     route2, r2_length, elevation2 = Astar(
+#         source, dest, 1, r1_length*1.5, place)
 
-    if (route1 == route2):
-        print("same")
-    else:
-        print("different")
-    print("done!")
+#     if (route1 == route2):
+#         print("same")
+#     else:
+#         print("different")
+#     print("done!")
--- src/server/utils.py
+++ src/server/utils.py
@@ -1,3 +1,6 @@
+from math import *
+
+
 def get_length(G, start, end):
     return G.edges[start, end, 0]['length']
 
@@ -18,7 +21,50 @@ def get_path_elevation(G, node_list):
     return total_elevation
 
 
+def get_route_coord(G, node_list):
+    routes = []
+    for node in node_list:
+        temp = [G.nodes()[node]['x'], G.nodes()[node]['y']]
+        routes.append(temp)
+    return routes
+
+
 def get_elevation_gain(G, start, end):
     if start == end:
         return 0
-    return G.nodes[start]['elevation'] - G.nodes[end]['elevation']
+    return G.nodes()[start]['elevation'] - G.nodes()[end]['elevation']
+
+
+def get_heuristic_distance(G, node1, node2):
+    n1 = G.nodes()[node1]
+    n2 = G.nodes()[node2]
+    R = 6737000
+    dlon = n1['y'] - n2['y']
+    dlat = n1['x'] - n2['x']
+    a = (sin(dlat/2))**2 + cos(n1['x']) * cos(n2['x']) * (sin(dlon/2))**2
+    c = 2 * atan2(sqrt(a), sqrt(1-a))
+    return R * c
+    # return sqrt((n1['x'] - n2['x']) ** 2 + (n1['y'] - n2['y']) ** 2)
+
+
+class NodeWrapper(object):
+    def __init__(self, node, parent=None, curr_dist=0, pred_distance=0, elevation=0, route_path=None):
+        self.id = node
+        self.parent = parent
+        self.curr_dist = curr_dist
+        self.elevation = elevation
+        self.route_path = route_path
+        self.pred_dist = pred_distance
+
+    def __lt__(self, other):
+        """Operation override for the less than operation to compare two objects of the same type.
+
+        Args:
+            other: another object of the NodeIdWrapper time, with which current object has to be compared
+
+        Returns:
+            A boolean representing if the current node is less than the other node
+        """
+        self_heuristic_dist = self.elevation
+        other_heuristic_dist = other.elevation
+        return self_heuristic_dist > other_heuristic_dist
